424928b3ac1b382a07f9b8dec2bf5af1e1a89015
Lars <larspeterjeppesen@gmail.com>
2025-08-16 15:57:26 -0300

    update to server and serversetup

--- RTServer/src/main.rs
+++ RTServer/src/main.rs
@@ -2,10 +2,15 @@
 
 #[get("/")]
 fn index() -> &'static str {
-    "Hello, world!"
+"Peer
+Prerequisites: Fellowship 30
+Talent Groups: Academics, Adeptus Arbites, Adeptus Mechanicus, Administratum, Astropaths, Ecclesiarchy, Feral Worlders, Government, Hivers, Inquisition, Middle Classes, Military, Nobility, the Insane, Underworld, Void Born, Workers.
+The Explorer is adept at dealing with a particular social group or organisation. He gains a +10 bonus to all Fellowship Tests when interacting with the chosen group.
+"
+    
 }
 
 #[launch]
-fn rocket() -> _ {
+fn rocket() -> Rocket<Build> {
     rocket::build().mount("/", routes![index])
 }

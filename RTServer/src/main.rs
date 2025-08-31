218835d432260cd44d9ff60b323cdbb4c5dce62d
Lars <larspeterjeppesen@gmail.com>
2025-08-31 14:38:49 -0300

    correction to previous commit

--- RTServer/src/main.rs
+++ RTServer/src/main.rs
@@ -64,7 +64,7 @@ Prerequisites: Fellowship 30
 Talent Groups: Academics, Adeptus Arbites, Adeptus Mechanicus, Administratum, Astropaths, Ecclesiarchy, Feral Worlders, Government, Hivers, Inquisition, Middle Classes, Military, Nobility, the Insane, Underworld, Void Born, Workers.
 The Explorer is adept at dealing with a particular social group or organisation. He gains a +10 bonus to all Fellowship Tests when interacting with the chosen group.    
     
-<pre id="Peer">
+<pre id=\"Peer\">
 Prerequisites: Fellowship 30
 Talent Groups: Academics, Adeptus Arbites, Adeptus Mechanicus, Administratum, Astropaths, Ecclesiarchy, Feral Worlders, Government, Hivers, Inquisition, Middle Classes, Military, Nobility, the Insane, Underworld, Void Born, Workers.
 The Explorer is adept at dealing with a particular social group or organisation. He gains a +10 bonus to all Fellowship Tests when interacting with the chosen group.    

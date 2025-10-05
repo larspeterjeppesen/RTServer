55a6c290c4f606f823d081a537b657ebb463611e
Lars <larspeterjeppesen@gmail.com>
2025-10-05 13:56:06 +0200

    cleaning up compiler warning

--- RTServer/src/main.rs
+++ RTServer/src/main.rs
@@ -11,14 +11,7 @@ const SOURCE_FILEPATH : &str= "data/RT_talents.txt";
 fn index() -> (ContentType, String) {
     let mut source_file = File::open(SOURCE_FILEPATH).unwrap();
     let mut content = String::new();
-    source_file.read_to_string(&mut content);
-    // println!("{content}");
-    // let content = "<h1>Peer</h1>   
-// <pre id=\"Peer\">
-// Prerequisites: Fellowship 30
-// Talent Groups: Academics, Adeptus Arbites, Adeptus Mechanicus, Administratum, Astropaths, Ecclesiarchy, Feral Worlders, Government, Hivers, Inquisition, Middle Classes, Military, Nobility, the Insane, Underworld, Void Born, Workers.
-// The Explorer is adept at dealing with a particular social group or organisation. He gains a +10 bonus to all Fellowship Tests when interacting with the chosen group.    
-// </pre>";
+    let _ = source_file.read_to_string(&mut content);
     (ContentType::HTML, content)
 }
 
